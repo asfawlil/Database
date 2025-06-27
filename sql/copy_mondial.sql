e512d66d4b2d1c29cf149e1afc644e244e6b35ca
Njomza Bytyqi <bytyqinj@hs-pforzheim.de>
2025-06-27 17:34:07 +0200

    Delete sql/copy_mondial.sql

--- sql/copy_mondial.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS student37.my_country;
-DROP TABLE IF EXISTS student37.my_countrypops;
-create table student37.my_country as select * from country;
-create table student37.my_countrypops as select * from countrypops;
