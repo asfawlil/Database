35e91e29d36f215be85f1e3e68e7c8d6563c1800
Njomza Bytyqi <bytyqinj@hs-pforzheim.de>
2025-06-27 17:33:05 +0200

    Update 5_advanced_features.sql

--- sql/5_advanced_features.sql
+++ sql/5_advanced_features.sql
@@ -1,37 +1,3 @@
-
-SELECT country, 
-       export_amount, 
-       import_amount, 
-       ABS(export_amount - import_amount) AS trade_gap
-FROM country_trade_industry
-WHERE year = 2023
-ORDER BY trade_gap ASC
-LIMIT 3;
-
-
-SELECT country, 
-       industry_share_percent, 
-       export_amount
-FROM country_trade_industry
-WHERE year = 2023
-ORDER BY industry_share_percent DESC, export_amount ASC
-LIMIT 3;
-
-
-
-SELECT country, export_products, import_products
-FROM country_trade_industry
-WHERE year = 2023
-  AND NOT EXISTS (
-    SELECT 1
-    FROM regexp_split_to_table(export_products, ', ') AS e
-    INTERSECT
-    SELECT 1
-    FROM regexp_split_to_table(import_products, ', ') AS i
-  );
-
- 
- 
 /* -----------------------------------------------------------------------------
    FILE:    5_advanced_features.sql
    PURPOSE: Add normalization and consistency constraints to currency, gdp, and inflation tables
